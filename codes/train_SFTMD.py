9f3f4829e6dff36f7f959c6b598fe2bfdc9a2f00
yjchai <929743545@qq.com>
2019-09-17 09:16:34 +0800

    Update train_SFTMD.py

--- codes/train_SFTMD.py
+++ codes/train_SFTMD.py
@@ -229,7 +229,7 @@ def main():
                 # log
                 logger.info('# Validation # PSNR: {:.4e}'.format(avg_psnr))
                 logger_val = logging.getLogger('val')  # validation logger
-                logger_val.info('<epoch:{:3d}, iter:{:8,d}> psnr: {:.4e}'.format(epoch, current_step, avg_psnr))
+                logger_val.info('<epoch:{:3d}, iter:{:8,d}> psnr: {:.6f}'.format(epoch, current_step, avg_psnr))
                 # tensorboard logger
                 if opt_F['use_tb_logger'] and 'debug' not in opt_F['name']:
                     tb_logger.add_scalar('psnr', avg_psnr, current_step)
