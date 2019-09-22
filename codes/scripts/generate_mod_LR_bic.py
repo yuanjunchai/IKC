c0b051a543b8d44d707dfb372310230a51ce5ba1
levi0809 <allen092911@gmail.com>
2019-09-22 22:55:02 +0800

    update

--- codes/scripts/generate_mod_LR_bic.py
+++ codes/scripts/generate_mod_LR_bic.py
@@ -17,17 +17,21 @@ def generate_mod_LR_bic():
     up_scale = 4
     mod_scale = 4
     # set data dir
-    sourcedir = '/mnt/yjchai/SR_data/Flickr2K/Flickr2K_HR' #'/mnt/yjchai/SR_data/DIV2K_test_HR' #'/mnt/yjchai/SR_data/Flickr2K/Flickr2K_HR'
-    savedir = '/mnt/yjchai/SR_data/Flickr2K_train' #'/mnt/yjchai/SR_data/DIV2K_test' #'/mnt/yjchai/SR_data/Flickr2K_train'
+    sourcedir = '/mnt/yjchai/SR_data/BSDS100' #'/mnt/yjchai/SR_data/DIV2K_test_HR' #'/mnt/yjchai/SR_data/Flickr2K/Flickr2K_HR'
+    savedir = '/mnt/yjchai/SR_data/BSDS100_test' #'/mnt/yjchai/SR_data/DIV2K_test' #'/mnt/yjchai/SR_data/Flickr2K_train'
+
+    # set random seed
+    util.set_random_seed(0)
 
     # load PCA matrix of enough kernel
     print('load PCA matrix')
-    pca_matrix = torch.load('media/sdc/yjchai/IKC/codes/pca_matrix.pth', map_location=lambda storage, loc: storage)
+    pca_matrix = torch.load('/media/sdc/yjchai/IKC/codes/pca_matrix.pth')
     print('PCA matrix shape: {}'.format(pca_matrix.shape))
 
     saveHRpath = os.path.join(savedir, 'HR', 'x' + str(mod_scale))
     saveLRpath = os.path.join(savedir, 'LR', 'x' + str(up_scale))
     saveBicpath = os.path.join(savedir, 'Bic', 'x' + str(up_scale))
+    saveLRblurpath = os.path.join(savedir, 'LRblur', 'x' + str(up_scale))
 
     if not os.path.isdir(sourcedir):
         print('Error: No source data found')
@@ -41,6 +45,8 @@ def generate_mod_LR_bic():
         os.mkdir(os.path.join(savedir, 'LR'))
     if not os.path.isdir(os.path.join(savedir, 'Bic')):
         os.mkdir(os.path.join(savedir, 'Bic'))
+    if not os.path.isdir(os.path.join(savedir, 'LRblur')):
+        os.mkdir(os.path.join(savedir, 'LRblur'))
 
     if not os.path.isdir(saveHRpath):
         os.mkdir(saveHRpath)
@@ -57,11 +63,16 @@ def generate_mod_LR_bic():
     else:
         print('It will cover ' + str(saveBicpath))
 
-    kernel_list = []
+    if not os.path.isdir(saveLRblurpath):
+        os.mkdir(saveLRblurpath)
+    else:
+        print('It will cover '+ str(saveLRblurpath))
 
     filepaths = sorted([f for f in os.listdir(sourcedir) if f.endswith('.png')])
     print(filepaths)
     num_files = len(filepaths)
+
+    kernel_map_tensor = torch.zeros((num_files, 1, 10)) # each kernel map: 1*10
 
     # prepare data with augementation
     for i in range(num_files):
@@ -69,19 +80,6 @@ def generate_mod_LR_bic():
         print('No.{} -- Processing {}'.format(i, filename))
         # read image
         image = cv2.imread(os.path.join(sourcedir, filename))
-        # gaussian random kernel
-        if up_scale == 2:
-            prepro = util.SRMDPreprocessing(up_scale, pca_matrix, para_input=15, kernel=21, noise=False, cuda=True,
-                                        sig_min=0.2, sig_max=2.0, rate_iso=1.0, scaling=3,
-                                        rate_cln=0.2, noise_high=0.0)
-        elif up_scale == 3:
-            prepro = util.SRMDPreprocessing(up_scale, pca_matrix, para_input=15, kernel=21, noise=False, cuda=True,
-                                        sig_min=0.2, sig_max=3.0, rate_iso=1.0, scaling=3,
-                                        rate_cln=0.2, noise_high=0.0)
-        elif up_scale == 4:
-            prepro = util.SRMDPreprocessing(up_scale, pca_matrix, para_input=15, kernel=21, noise=False, cuda=True,
-                                        sig_min=0.2, sig_max=4.0, rate_iso=1.0, scaling=3,
-                                        rate_cln=0.2, noise_high=0.0)
 
         width = int(np.floor(image.shape[1] / mod_scale))
         height = int(np.floor(image.shape[0] / mod_scale))
@@ -90,19 +88,28 @@ def generate_mod_LR_bic():
             image_HR = image[0:mod_scale * height, 0:mod_scale * width, :]
         else:
             image_HR = image[0:mod_scale * height, 0:mod_scale * width]
-        image_HR_blurred = cv2.filter2D(image_HR, -1, blur_ker)
+        img_HR = util.img2tensor(image_HR)
+        C, H, W = img_HR.size()
+        # LR_blur, by random gaussian kernel
+        prepro = util.SRMDPreprocessing(up_scale, pca_matrix, para_input=10, kernel=21, noise=False, cuda=True,
+                                        sig_min=0.2, sig_max=4.0, rate_iso=1.0, scaling=3,
+                                        rate_cln=0.2, noise_high=0.0)
+        LR_img, ker_map = prepro(img_HR.view(1, C, H, W))
+        image_LR_blur = util.tensor2img(LR_img)
         # LR
-        image_LR = imresize_np(image_HR_blurred, 1 / up_scale, True)
+        image_LR = imresize_np(image_HR, 1 / up_scale, True)
         # bic
         image_Bic = imresize_np(image_LR, up_scale, True)
 
         cv2.imwrite(os.path.join(saveHRpath, filename), image_HR)
         cv2.imwrite(os.path.join(saveLRpath, filename), image_LR)
         cv2.imwrite(os.path.join(saveBicpath, filename), image_Bic)
-        kernel_list.append(blur_ker)
+        cv2.imwrite(os.path.join(saveLRblurpath, filename), image_LR_blur)
 
-
-    print("Down smaple Done: X"+str(up_scale))
+        kernel_map_tensor[i] = ker_map
+    # save dataset corresponding kernel maps
+    torch.save(kernel_map_tensor, './BSDS100_kermap.pth')
+    print("Image Blurring & Down smaple Done: X"+str(up_scale))
 
 if __name__ == "__main__":
     generate_mod_LR_bic()
